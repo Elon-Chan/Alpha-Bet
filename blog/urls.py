c8965f98499285be1bc62212ec1a197e51afd6ef
elon_chan <elonchan1998@gmail.com>
2021-02-22 00:04:46 +0800

    finished tutorial code (exclude set up server part

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 from blog.dash_apps.finished_apps import simple_example, candlestick_example # not used but must be imported in order to work
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
     path('dash/', views.dash, name='blog-dash'),
     path('candlestick/', views.candlestick, name='blog-dash')
